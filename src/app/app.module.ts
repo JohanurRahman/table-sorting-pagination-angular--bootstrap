50539f6b051fd401630ebc5722de0676c916481a
JohanurRahman <johanur.rahman@gmail.com>
2019-06-18 14:36:45 +0600

    Added Basic Folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ApiTableComponent } from './component/api-table/api-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiTableComponent
   ],
   imports: [
     BrowserModule
